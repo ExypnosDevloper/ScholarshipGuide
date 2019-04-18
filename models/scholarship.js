b0241762aa9ac43d6e576811f87c7e31435ae479
ExypnosDevloper <grubnoreplygooogle@gmail.com>
2019-04-18 10:00:14 +0530

    Add files via upload

---
+++ models/scholarship.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    //imagePath: { type: String, required: true},
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    caste: {type: String, required:true},
+    education: {type: String,required:true},
+    salary: {type: Number, required: true}
+});
+
+module.exports = mongoose.model('Scholarship', schema);
